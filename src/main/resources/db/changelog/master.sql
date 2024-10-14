25364c4a4709e2e3d502dc1e8a713192fc5cbf35
Administrator <zakharchuksasha7@gmail.com>
2024-10-14 06:48:07 -0700

    added master.sql

--- src/main/resources/db/changelog/master.sql
+++ src/main/resources/db/changelog/master.sql
@@ -1,13 +1,9 @@
 --liquibase formatted sql
 
-
 --changeset Owl:create_enum_types
 CREATE TYPE sleeve_type_enum AS ENUM ('SHORT', 'LONG', 'SLEEVELESS');
-
 CREATE TYPE size_enum AS ENUM ('S', 'M', 'L', 'XL');
-
 CREATE TYPE material_enum AS ENUM ('COTTON', 'LEATHER', 'POLYESTER', 'NYLON', 'DENIM', 'WOOL', 'FLEECE', 'SUEDE', 'LINEN', 'VINYL');
-
 
 --changeset Owl:create_table_shirt
 --include file="db/changelog/sql/20241009_create_table_shirt.sql"
